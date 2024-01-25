17a1a2bd2d621c1d447971725d575e72c28582ad
MDAkhtar123 <akhtar.java5@gmail.com>
2024-01-25 12:57:31 +0530

    first commit

---
+++ src/main/java/com/accenture/HelloSpringBoot/HelloSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.accenture.HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+		System.out.println();
+
+
+
+
+
+
+
+
+	}
+
+}
---
+++ src/test/java/com/accenture/HelloSpringBoot/Hello/Spring/Boot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.HelloSpringBoot.Hello.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
